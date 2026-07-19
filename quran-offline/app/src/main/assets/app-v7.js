a4d15f230b5e7b77216a8f9ccc077811d761f61e
Godnit <gema.123.9000@gmail.com>
2026-07-19 20:20:31 +0300

    Load fully offline audio interface

--- quran-offline/app/src/main/assets/app-v7.js
+++ quran-offline/app/src/main/assets/app-v7.js
@@ -5,8 +5,10 @@
   document.write('<link rel="stylesheet" href="app-v8.css">');
   document.write('<link rel="stylesheet" href="app-v11.css">');
   document.write('<link rel="stylesheet" href="app-v12.css">');
+  document.write('<link rel="stylesheet" href="app-v13.css">');
   document.write('<script src="app-v8.js"><\/script>');
   document.write('<script src="app-v9.js"><\/script>');
   document.write('<script src="app-v11.js"><\/script>');
   document.write('<script src="app-v12.js"><\/script>');
+  document.write('<script src="app-v13.js"><\/script>');
 })();
