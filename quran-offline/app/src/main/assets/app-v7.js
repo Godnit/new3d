616b54aa1d6e1ccc71dfba0d6cfa41046a614a90
Godnit <gema.123.9000@gmail.com>
2026-07-19 15:50:48 +0300

    Load v4.6 UI and native bridge integration

--- quran-offline/app/src/main/assets/app-v7.js
+++ quran-offline/app/src/main/assets/app-v7.js
@@ -3,10 +3,10 @@
 
   /* Compatibility loader: index.html already loads this file synchronously. */
   document.write('<link rel="stylesheet" href="app-v8.css">');
-  document.write('<link rel="stylesheet" href="app-v10.css">');
   document.write('<link rel="stylesheet" href="app-v11.css">');
+  document.write('<link rel="stylesheet" href="app-v12.css">');
   document.write('<script src="app-v8.js"><\/script>');
   document.write('<script src="app-v9.js"><\/script>');
-  document.write('<script src="app-v10.js"><\/script>');
   document.write('<script src="app-v11.js"><\/script>');
+  document.write('<script src="app-v12.js"><\/script>');
 })();
