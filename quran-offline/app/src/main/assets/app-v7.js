a5ed2b71e85ca0d92b7ca6bc626b243ad12c4067
Godnit <gema.123.9000@gmail.com>
2026-07-22 18:49:21 +0300

    Load v4.15 phone fixes after existing compatibility layers

--- quran-offline/app/src/main/assets/app-v7.js
+++ quran-offline/app/src/main/assets/app-v7.js
@@ -10,6 +10,7 @@
   document.write('<link rel="stylesheet" href="app-v15.css">');
   document.write('<link rel="stylesheet" href="app-v16.css">');
   document.write('<link rel="stylesheet" href="app-v17.css">');
+  document.write('<link rel="stylesheet" href="app-v18.css">');
   document.write('<script src="app-v8.js"><\/script>');
   document.write('<script src="app-v9.js"><\/script>');
   document.write('<script src="app-v11.js"><\/script>');
@@ -19,4 +20,5 @@
   document.write('<script src="app-v15.js"><\/script>');
   document.write('<script src="app-v16.js"><\/script>');
   document.write('<script src="app-v17.js"><\/script>');
+  document.write('<script src="app-v18.js"><\/script>');
 })();
